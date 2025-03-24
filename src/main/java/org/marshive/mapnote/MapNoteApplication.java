cd5f1755ebc23cc7edf7ce806e12548b340e9566
Qhbee <qhbee@qq.com>
2025-03-25 00:54:55 +0800

    feat: 初始化 MapNote 项目- 创建 Spring Boot 项目结构和基本配置 - 添加 Redis、MyBatis 和 Lombok 依赖- 设置 Maven 构建配置- 添加基本的 Application 类和测试类 - 设置 .gitignore 和 .gitattributes 文件 - 添加 Maven Wrapper 配置文件和脚本

---
+++ src/main/java/org/marshive/mapnote/MapNoteApplication.java
@@ -0,0 +1,16 @@
+package org.marshive.mapnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Qhbee
+ */
+@SpringBootApplication
+public class MapNoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapNoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/marshive/mapnote/MapNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.marshive.mapnote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapNoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
